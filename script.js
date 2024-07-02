19d214ba2ee1e90f691438f62e57a33f17a0555a
Elliot <ellthrowaway2@icloud.com>
2024-07-02 18:21:34 -0400

    bubble sort logic complete

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ const sortButton = document.getElementById("sort");
 const bubbleSort = (array) => {
     for (let i = 0; i < array.length; i++){
         for (let j = 0; j < array.length - 1; j++){
-            console.log(array, array[j], array[j+1])
+            // console.log(array, array[j], array[j + 1])
+            if (array[j] > array[j + 1]) {
+                const temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+            }
         }
     }
+    return array
 };
 
 
@@ -15,7 +21,8 @@ const bubbleSort = (array) => {
 const sortInputArray = (e) => {
     e.preventDefault();
     const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value))
-    updateUI(inputValues)
+    const sortedValues = bubbleSort(inputValues)
+    updateUI(sortedValues)
 };
 
 const updateUI = (array = []) => {
